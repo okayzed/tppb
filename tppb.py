56b5826370ab5b3cd9e4be5f3ca8c27f28f6d3e4
okay <okay@chert>
2016-08-18 08:58:15 -0700

    add loop breaker and fast variance calculations
    * don't move partition indeces unless they improve things, too
    
    * converge faster

--- tppb.py
+++ tppb.py
@@ -2,12 +2,17 @@ import time
 import math
 import sys
 import random
+import copy
 
 DEBUG_SUMS=False
 MAX_ITERATIONS=10000
 MAX_SECONDS=3
 BEST_CANDIDATE_ONLY=True
-
+LOOP_BREAKER=True
+MAX_LOOPS=50
+FAST_VALUATIONS=True
+STEP_SIZE=1
+ITER_TIME=0.5
 
 class ThibM(list):
   def adjusting_left_split(self, arr, k):
@@ -82,7 +87,7 @@ class ThibM(list):
 
     return proper_indices
 
-  def split_array(self, arr, k):
+  def center_split_array(self, arr, k):
     left_idx = 0
     right_idx = len(arr) - 1
 
@@ -103,20 +108,20 @@ class ThibM(list):
         left_idx += 1
         if left_sum >= bucket_size:
           remaining_partitions -= 1
-          bucket_size = max(remaining_sum / remaining_partitions, 1)
-          indeces.append(left_idx)
           left_sum -= bucket_size
+          indeces.append(left_idx)
+          indeces.sort()
       else:
         remaining_sum -= arr[right_idx]
         right_sum += arr[right_idx]
         right_idx -= 1
         if right_sum >= bucket_size:
+          remaining_sum -= right_sum
           remaining_partitions -= 1
-          bucket_size = max(remaining_sum / remaining_partitions, 1)
-          indeces.append(right_idx)
           right_sum -= bucket_size
+          indeces.append(right_idx+1)
+          indeces.sort()
 
-    indeces.sort()
     return indeces
 
   def build_prefix_table(self):
@@ -135,7 +140,7 @@ class ThibM(list):
       subarrs.append(arr[prev_index:index])
       prev_index = index
 
-    if prev_index < len(arr):
+    if prev_index < len(arr) - 1 and len(indeces) > 0:
       subarrs.append(arr[prev_index:])
 
     return subarrs
@@ -157,6 +162,40 @@ class ThibM(list):
     index = indeces[i]-1
     return self.prefix_sums[index] - self.prefix_sums[prev_index]
 
+  def fast_evaluate(self, indeces, k, adjusted=[]):
+    if not hasattr(self, "fast_sums_cache") or False:
+      self.fast_sum_total = 0
+      self.fast_sums_cache = {}
+      adjusted = range(len(indeces))
+
+    avg_sum = self.prefix_sums[-1] / k
+    new_values = {}
+    for i in adjusted:
+      if i not in new_values:
+        new_values[i] = self.get_partition_sum(indeces, i)
+      if i-1 >= 0 and i-1 not in new_values:
+        new_values[i-1] = self.get_partition_sum(indeces, i-1)
+      if i+1 < len(indeces) and i+1 not in new_values:
+        new_values[i+1] = self.get_partition_sum(indeces, i+1)
+
+    for index in new_values:
+      new_val = (avg_sum - new_values[index])**2
+      old_val = 0
+
+      if index in self.fast_sums_cache:
+        old_val = self.fast_sums_cache[index]
+
+      self.fast_sums_cache[index] = new_val
+
+      self.fast_sum_total -= old_val
+      self.fast_sum_total += new_val
+
+
+    return math.sqrt(self.fast_sum_total / k)
+
+
+
+
   def evaluate_partitions(self, indeces, k, use_cache=False):
     arr = self
     self.build_prefix_table()
@@ -176,7 +215,7 @@ class ThibM(list):
 
     start_indeces = []
     methods = "adjusting left", "constant left split", "adjusting right", "constant right split", "adjusting center split"
-    for i,func in enumerate([self.adjusting_left_split, self.left_split, self.adjusting_right_split, self.right_split, self.split_array]):
+    for i,func in enumerate([self.adjusting_left_split, self.left_split, self.adjusting_right_split, self.right_split, self.center_split_array]):
       indeces = func(arr, k)
 
       # VERY IMPORTANT TO SORT INDECES
@@ -200,6 +239,9 @@ class ThibM(list):
     for data in start_indeces:
         print "USING METHOD", data[2]
         indeces = data[1]
+        original_indeces = copy.copy(indeces)
+        best_val = sys.maxint
+        best_indeces = copy.copy(indeces)
 
         if len(indeces) != k - 1:
           print "LEN INDECES", len(indeces)
@@ -210,50 +252,71 @@ class ThibM(list):
 
         prev_val = starting
         last_opt = starting
+        last_move = {}
         threshold = 1
+        loop_count = 0
         success = False
         starting_std = self.evaluate_partitions(indeces, k)
-        print "STARTING STD", starting_std
+        starting_faststd = self.fast_evaluate(indeces, k, [])
+        print "STARTING STD", starting_std, "FAST", starting_faststd
         start = time.time()
         prev = start
         moves = 0
 
 
-        self.results.append([indeces, data[2], starting_std])
         for i in xrange(MAX_ITERATIONS):
           self.iterations = i
           indeces.sort()
           movements = self.find_likely_permutation(indeces, k)
           moved = {}
+          changes = 0
 
           num_moves = int(math.log(len(self)))
           for val,index,change in movements[:num_moves]:
-            now = time.time()
-            moves += 1
-
             if not index in moved:
               moved[index] = True
             else:
               continue
 
 
-            if now - prev > 1:
-              print "ON ITERATION", i, "MOVES", moves, "IMPROVEMENT IS %.02f%% OF ORIGINAL" % (val / float(starting_std) * 100)
+            now = time.time()
+            moves += 1
+            if now - prev > ITER_TIME:
+              print "ON ITERATION", i, "MOVES", moves, \
+                "ESTIMATED IMPROVEMENT IS %.02f%% OF ORIGINAL" % (val / float(starting_std) * 100)
               prev = now
               print "PREV OPTIMUM", last_opt, "NEW OPT", val, "DELTA", (last_opt - val)
 
-              if last_opt == val:
+              if last_opt <= val:
                 print "LIKELY LOCAL, STOPPING FOR NOW"
                 success = True
                 break
               last_opt = val
 
-            indeces[index] += change
-            if prev_val <= val:
-              print "REACHED LOCAL LOCAL MINIMA AFTER", len(moved), "MOVES IN ITERATION %s, RECALCULATING MOVES" % (i)
+            if prev_val <= val or last_opt <= val:
+              print "REACHED LOCAL LOCAL MINIMA AFTER", len(moved), \
+                "MOVES IN ITERATION %s, RECALCULATING MOVES" % (i)
 
               success = True
               break
+
+            # dont change the indeces until we are sure it improves things
+            indeces[index] += change
+            if val < best_val:
+              best_val = val
+              best_indeces = copy.copy(indeces)
+
+            if LOOP_BREAKER and index in last_move and last_move[index] + change == 0:
+              # we need to recognize this likely loop and if nothing else has changed...
+              loop_count += 1
+              if loop_count >= MAX_LOOPS:
+                print "LIKELY LOOP, BREAKING THE CYCLE"
+                success = True
+                break
+
+            last_move[index] = change
+            changes += 1
+
 
 
           if now - start > MAX_SECONDS:
@@ -263,21 +326,34 @@ class ThibM(list):
           if success:
             movements = self.find_likely_permutation(indeces, k)
             val, index, change = movements[0]
-            indeces[index] += change
-            val = self.evaluate_partitions(indeces, k)
-            if prev_val > val:
+            if index in last_move and last_move[index] + change == 0 and loop_count >= MAX_LOOPS:
+              print "FORCE BREAKING LOOP"
+              break
+
+            if prev_val > val and last_opt > val and changes > 1:
               success = False
+              loop_count = 0
             else:
               break
+
+            # dont make change unless we think it really makes things better
+            indeces[index] += change
+            val = self.evaluate_partitions(indeces, k)
+
           prev_val = val
 
+        if now - start > MAX_SECONDS:
+          break
         if success:
           print "CONVERGED ON LOCAL OPTIMUM AFTER", i, "ITERATIONS AND", moves, "MOVES"
         else:
           print 'COULDNT CONVERGE ON OPTIMUM AFTER %s ITERATIONS AND %s SECONDS' % (self.iterations, int(now - start))
 
+        val = self.evaluate_partitions(best_indeces, k)
+        self.results.append([best_indeces, name, starting_std])
+        self.results.append([original_indeces, "start " + name, starting_std])
         if not BEST_CANDIDATE_ONLY:
-          self.print_stats(indeces, k, starting_std)
+          self.print_stats(best_indeces, k, starting_std)
 
         if BEST_CANDIDATE_ONLY:
           break
@@ -290,7 +366,7 @@ class ThibM(list):
   def find_likely_permutation(self, indeces, k):
     movements = []
 
-    steps = 3
+    steps = STEP_SIZE
     self.evaluate_partitions(indeces, k)
     for i, pos in enumerate(indeces):
       min_pos = 0
@@ -302,14 +378,23 @@ class ThibM(list):
         max_pos = (indeces[i+1]-1 + indeces[i]) / 2
 
       for j in xrange(min_pos, max_pos, steps):
-        indeces[i] = j
-        val = self.evaluate_partitions(indeces, k)
-        movements.append([val, i, j-pos])
+        if j == pos:
+          continue
 
+        indeces[i] = j
+        if FAST_VALUATIONS:
+          fastval = self.fast_evaluate(indeces, k, [i])
+          movements.append([fastval, i, j-pos])
+        else:
+          val = self.evaluate_partitions(indeces, k)
+          movements.append([val, i, j-pos])
+
+      self.fast_evaluate(indeces, k, [i])
       indeces[i] = pos
 
 
     movements.sort()
+
     return movements
 
   def print_stats(self, indeces, k, starting_std):
@@ -375,6 +460,7 @@ def main():
     print "GENERATING %s %s SAMPLES" % (n, method)
     print "CREATING %s PARTITIONS" % k
 
+    start = time.time()
     t = ThibM(l)
 
     try:
@@ -388,7 +474,9 @@ def main():
     t.results.sort(key=lambda x: t.evaluate_partitions(x[0], k))
     data = t.results[0]
     print "BEST RESULTS %s" % data[1]
+    now = time.time()
     t.print_stats(data[0], k, data[2])
+    print "TOTAL TIME: %s seconds" % (now - start)
     print "\n\n\n"
 
 if __name__ == "__main__":
